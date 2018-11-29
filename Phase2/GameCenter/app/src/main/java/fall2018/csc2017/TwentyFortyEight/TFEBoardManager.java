da7cd16f4fa941c4eedfb2535cfce8b773ff8d9b
xiekai3 <michaelk.xie@mail.utoronto.ca>
2018-11-29 00:03:59 -0500

    Javadocced all classes and methods in 2048

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/TwentyFortyEight/TFEBoardManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/TwentyFortyEight/TFEBoardManager.java
@@ -4,19 +4,44 @@ import java.io.Serializable;
 
 public class TFEBoardManager implements Serializable {
 
+    /**
+     * Current 2048 board.
+     */
     private TFEBoard board;
+
+    /**
+     * Number of total undoes allowed.
+     */
     int undos = 3;
+
+    /**
+     * Constructor for a new board manager.
+     * @param board - board to be managed.
+     */
     TFEBoardManager(TFEBoard board) {
         this.board = board;
     }
 
-
+    /**
+     * Getter for number of undoes.
+     * @return number of undoes remaining.
+     */
     int getUndos(){
         return undos;
     }
+
+    /**
+     * Setter for number of undoes
+     * @param i - number of undoes allowed.
+     */
     void setUndos(int i){
         this.undos = i;
     }
+
+    /**
+     * Getter for current board state.
+     * @return current board being managed.
+     */
     public TFEBoard getBoard(){return this.board;}
 
 
