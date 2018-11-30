242db2e67808a8c4cac4faa73a45fcc883db6753
xiekai3 <michaelk.xie@mail.utoronto.ca>
2018-11-30 03:36:20 -0500

    Added a citation for slide gesture code in 2048

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/twenty_forty_eight/TFETile.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/twenty_forty_eight/TFETile.java
@@ -12,29 +12,34 @@ class TFETile implements Serializable {
     /**
      * Constructor for TFETile.
      */
-    TFETile(int value){
+    TFETile(int value) {
         tileValue = value;
     }
 
     /**
      * Setter for value of a tile.
+     *
      * @param newValue - new value of the tile.
      */
-    void TFEvaluesetter(int newValue){
+    void TFEvaluesetter(int newValue) {
         tileValue = newValue;
     }
 
     /**
      * Getter for value of a tile
+     *
      * @return value of the tile.
      */
-    int getTileValue(){return tileValue;}
+    int getTileValue() {
+        return tileValue;
+    }
 
     /**
      * Copy function to generate a new tile with same value as current tile.
+     *
      * @return new tile with same value as current tile.
      */
-    TFETile copy(){
+    TFETile copy() {
         return new TFETile(getTileValue());
     }
 
