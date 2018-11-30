a785a6ba124eb226ebaab7e7563429ca4ad07ec6
kevin <kevinyz.zhang@mail.utoronto.ca>
2018-11-30 02:25:47 -0500

    Added empty tfe tile unit test

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/twenty_forty_eight/TFETileTest.java
@@ -0,0 +1,25 @@
+package fall2018.csc2017.twenty_forty_eight;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TFETileTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void TFEvaluesetter() {
+    }
+
+    @Test
+    public void getTileValue() {
+    }
+
+    @Test
+    public void copy() {
+    }
+}
