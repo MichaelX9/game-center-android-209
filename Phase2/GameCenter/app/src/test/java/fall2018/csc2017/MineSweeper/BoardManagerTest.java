15e2482b6df7ff88e24d52ff741739654bb5f437
kevin <kevinyz.zhang@mail.utoronto.ca>
2018-11-29 23:40:39 -0500

    fixed small bug where processclick method failed on the first time running

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/MineSweeper/BoardManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/MineSweeper/BoardManagerTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
 public class BoardManagerTest {
     private Board mineBoard;
     private BoardManager minesweeperBM;
-    private MineSweeperScoreBoard mineScoreBoard;
+    private MineSweeperScoreBoard mineScoreBoard = null;
     private Context context;
 
     /**
